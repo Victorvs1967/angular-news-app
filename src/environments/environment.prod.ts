a937f4448d52a0822f48206b374253f20a2c743e
Victor <victorsmirnov@me.com>
2022-08-27 16:14:09 +0300

    add production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '&apiKey=75c2bb76933b4995ba27d29c4404c1bf',
+  baseUrl: 'https://newsapi.org/v2/',
+  sources: 'top-headlines/sources?language=',
+  headline: 'top-headlines?sources=',
+  everything: 'everything?q=',
+  ru: '&language=ru',
+  us: '&language=en',
 };
