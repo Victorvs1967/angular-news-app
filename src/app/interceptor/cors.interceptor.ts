8f52adc200a2581d70ba8cdc4e0bedd27370bf97
Victor <victorsmirnov@me.com>
2022-10-12 14:13:20 +0300

    add access-control-allow-

--- src/app/interceptor/cors.interceptor.ts
+++ src/app/interceptor/cors.interceptor.ts
@@ -15,7 +15,9 @@ export class CorsInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     request = request.clone({
       setHeaders: {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
       }
     });
     return next.handle(request);
